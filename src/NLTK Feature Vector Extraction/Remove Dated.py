f247f04dea942310e2a0960eefd52eddadbd953b
VarunJoshi10 <varunjoshi12363@gmail.com>
2023-10-28 20:27:41 +0530

    deleted jupyter files

--- src/NLTK Feature Vector Extraction/Remove Dated.py
+++
@@ -1,9 +0,0 @@
-def remDate_Space(file_name):
-    file = open('data.txt','r')
-    a = file.read()
-    b = a.split("Dated:")[0]
-    c = "".join(b.split("\n"))
-    file.close()
-    file = open('data.txt','w')
-    file.write(c)
-    file.close()
